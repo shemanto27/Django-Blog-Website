1f7b442df4538ca67540d3a77131c9d40cf5f45e
shemanto27 <shemantosharkarofficial@gmail.com>
2024-08-30 15:36:08 +0600

    recent post section added

--- blog/views.py
+++ blog/views.py
@@ -4,9 +4,11 @@ from blog.models import *
 # Create your views here.
 def home_page(request):
     posts = Post.objects.all().order_by("-created_on")
+    recent_posts = Post.objects.all().order_by("-created_on")[:4]
     context = {
         'posts' : posts,
-        'title' : "Shemanto's Blog"
+        'title' : "Shemanto's Blog",
+        'recent_posts' : recent_posts,
     }
     return render(request, 'home.html', context)
 
